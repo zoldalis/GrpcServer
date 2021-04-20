9ea6ba08214d163f5a001b5b05c580e2ef3e8713
Дудко Михаил <mikel2000@bk.ru>
2021-04-20 23:00:38 +0500

    1-st commit desktop

--- GrpcServer/Services/FileManager.cs
+++ GrpcServer/Services/FileManager.cs
@@ -2,18 +2,35 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Npgsql;
 
 namespace GrpcServer.Services
 {
     public class FileManager : IFileManager
     {
-        private static FileManager manager;
 
         private Dictionary<int, MyFile> files_storage;
         
         public FileManager()
         {
             files_storage = new Dictionary<int, MyFile>();
+
+            var cs = "Host=localhost;Username=postgres;Password=s$cret;Database=testdb";
+
+            using var con = new NpgsqlConnection(cs);
+            con.Open();
+
+            using var cmd = new NpgsqlCommand();
+            cmd.Connection = con;
+
+            cmd.CommandText = "DROP TABLE IF EXISTS files";
+            cmd.ExecuteNonQuery();
+
+            cmd.CommandText = @"CREATE TABLE files(id SERIAL PRIMARY KEY, 
+                    name VARCHAR(255), price INT)";
+
+            cmd.CommandText = "INSERT INTO files(name, price) VALUES('filename.dat',52642)";
+            cmd.ExecuteNonQuery();
         }
         
         public void FilePushBack(Id id,MyFile file)
